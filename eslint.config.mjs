f4ed235d9155929a0a81478f946b7806adcea7e5
Cody Butz <codyjbutz@gmail.com>
2025-09-29 10:47:22 -0400

    Trading Bot init

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['**/dist/*', '**/node_modules/*'],
+  },
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
+      {
+        languageOptions: {
+          parserOptions: {
+            projectService: true,
+            tsconfigRootDir: import.meta.dirname,
+          },
+        },
+      },
+    ],
+    rules: {
+      eqeqeq: 'error',
+      'no-unreachable': 'warn',
+      'no-use-before-define': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+);
